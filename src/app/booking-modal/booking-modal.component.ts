48b0206c13c75aa835ca239daf2756f54d375a98
bhakt <rohitrocks.bhagat@gmail.com>
2021-12-13 13:57:28 +0530

    AG-Grid Edit Form

--- src/app/booking-modal/booking-modal.component.ts
+++ src/app/booking-modal/booking-modal.component.ts
@@ -10,16 +10,33 @@ import { Booking } from '../model/booking';
 })
 export class BookingModalComponent implements OnInit,AfterViewInit{
   
-  
-  ngOnInit(): void {
-  }
+  senderName: any;
+  label: string;
+
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    
+    console.log(this.params.node.data)
+    this.model.senderName = this.params.node.data.senderName;
+    this.model.senderAddress = this.params.node.data.senderAddress;
+    this.model.senderCityName = this.params.node.data.senderCityName;
+    this.model.senderPinCode = this.params.node.data.senderPinCode;
+    this.model.senderMobileNumber = this.params.node.data.senderMobileNumber;
+    this.model.senderEmailId = this.params.node.data.senderEmailId;
+    this.model.receiverName = this.params.node.data.receiverName;
+    this.model.receiverAddress = this.params.node.data.receiverAddress;
+    this.model.receiverCityName = this.params.node.data.receiverCityName;
+    this.model.receiverPinCode = this.params.node.data.receiverPinCode;
+    this.model.receiverMobileNumber = this.params.node.data.receiverMobileNumber;
+    this.model.receiverEmailId = this.params.node.data.receiverEmailId;
+    this.model.category = this.params.node.data.category;
+    this.model.priority = this.params.node.data.priority;
+    this.model.userInstruction = this.params.node.data.userInstruction;
   }
 
   agInit(params: any): void {
     this.params = params;
+    this.label = this.params.label;
   }
 
   private params: any;
